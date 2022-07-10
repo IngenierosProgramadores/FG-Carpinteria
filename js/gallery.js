e10fd1a3af2c79b93eee9eb0e742fbad7742448f
Edgar Eduardo Meneses Barragán <edgar@wasp.mx>
2022-07-10 12:44:58 -0500

    modal 3.1

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {  
+$(document).ready(function() {
+  var $window = $(window);
   $( window ).resize(function() {
         let maskHeight = $(document).height();
         let maskWidth = $window.width();
         $('#mask').css({'width':maskWidth,'height':maskHeight});
+        let id = $('.gallery-picture').attr('href');
+        let winH = $window.height();
+        let winW = $window.width();
+        $(id).css('top',  winH/2-$(id).height()/2);
+        $(id).css('left', winW/2-$(id).width()/2);
   });
-  var $window = $(window);
   $('.gallery-picture').click(function(e) {
     //Cancel the link behavior
           e.preventDefault();
