d2b41563800a54612186f9b8c4edff5790373d30
Raul Belloso M <raul.belloso.m@gmail.com>
2022-07-30 12:09:41 -0500

    cambiar a php

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+if (isset($_POST['submit'])) {
+    $nombre = $_POST['fname'];
+    $correo = $_POST['email'];
+    $telefono = $_POST['tel'];
+    $comentarios = $_POST['comentarios'];
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $captcha = $_POST['g-recaptcha-response'];
+    $secretKey = "6LddijMhAAAAAJBFYNRsEHax7PyngJN_y4sWFna-";
+
+    $res = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$captcha&remoteip=$ip");
+
+    $atributos = json_decode($res, TRUE);
+
+    // $errors = array();
+    if (!$atributos['success']) {
+        $errors[] = 'Verificación captcha obligatoria';
+    }
+    if (count($errors) == 0) {
+        ini_set( 'display_errors', 1 );
+        error_reporting( E_ALL );
+
+        $from = 'admin@pvj.mx';
+        $to = "fgcarpinteria@pvj.mx";
+        $subject = "Contacto FG Carpinteria";
+
+        $message = "NOMBRE : " . $nombre  . ",\r\n";
+        $message .= "CORREO : " . $correo . " \r\n";
+        $message .= "TELEFONO : " . $telefono . " \r\n";
+        $message .= "IP : " . $ip . " \r\n";
+        $message .= "COMENTARIOS : " . $comentarios . " \r\n";
+
+        $headers = "From:" . $from . " \r\n";
+        $headers .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+        $headers .= "Mime-Version: 1.0 \r\n";
+        $headers .= "Content-Type: text/plain";
+        mail($to,$subject, utf8_decode($message), $headers);
+        echo "The email message was sent.";
+        
+        header("Location:https://fgcarpinteria.pvj.mx");
+    }
+
+  }
+?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -38,6 +83,26 @@
     ></script>
   </head>
   <body>
+  <?php
+            if (isset($errors)) {
+                if (count($errors) > 0) {
+            ?>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <?php
+                                foreach ($errors as $error) {
+                                    echo $error . '<br>';
+                                }
+                                ?>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        </div>
+                    </div>
+            <?php
+                }
+            }
+            ?>
     <header class="header">
       <nav class="nav container" id="nav">
         <a href="/" class="nav_logo">
@@ -388,7 +453,7 @@
           <div class="title-contacto">
             <h2>Contacto</h2>
           </div>
-          <form action="mail.php" method="post">
+          <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
             <label for="fname">Nombre completo</label>
             <input type="text" id="fname" name="fname" required />
             <label for="email">Correo electronico</label>
